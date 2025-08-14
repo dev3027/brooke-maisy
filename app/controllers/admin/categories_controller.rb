46e0a8510bdaa7ea9a5638deebe1588a121d2cb6
Mason Roberts <mrobe18@gmail.com>
2025-08-14 11:49:52 -0400

    fixed the find action on the set_category method to include slug.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -140,7 +140,7 @@ class Admin::CategoriesController < Admin::BaseController
   private
 
   def set_category
-    @category = Category.find(params[:id])
+    @category = Category.find_by!(slug: params[:id])
   end
 
   def category_params
